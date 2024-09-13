3343908e7a2577cc2c539d1b0d6acfd833f74f8a
yujan Thulung <yujanrai07@gmail.com>
2024-09-13 22:44:10 +0545

    Header and brand design completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+    return (
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit natus
+                    pariatur nam quam vitae exercitationem ullam assumenda architecto deserunt?
+                    Exercitationem?
+                </p>
 
-export default Header
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your email address." />
+                    <button type="button">Get Started</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="img" />
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+            </div>
+
+            <div className="gpt3__header-image">
+                <img src={ai} alt="img" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
