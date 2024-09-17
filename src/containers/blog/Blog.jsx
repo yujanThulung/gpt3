f2a518d8cd473eda8917ff24b7e5549042ae29f7
yujan Thulung <yujanrai07@gmail.com>
2024-09-17 19:20:36 +0545

    blog section complete

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './blog.css'
+import {Article} from '../../components';
+import {blog01,blog02,blog03,blog04,blog05} from './imports';
+
+let date = new Date();
+const options = {month:'short',day:'numeric',year:'numeric'};
+const formattedDate = date.toLocaleDateString('en-US', options).replace(',', '');
+
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className='
+      gpt3__blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imageUrl={blog01} date={formattedDate} title="Gpt-3 and Open AI is the future. Let us explore how it is?"/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+        <Article imageUrl={blog02} date={formattedDate} title="Gpt-3 and Open AI is the future. Let us explore how it is?"/>
+        <Article imageUrl={blog03} date={formattedDate} title="Gpt-3 and Open AI is the future. Let us explore how it is?"/>
+        <Article imageUrl={blog04} date={formattedDate} title="Gpt-3 and Open AI is the future. Let us explore how it is?"/>
+        <Article imageUrl={blog05} date={formattedDate} title="Gpt-3 and Open AI is the future. Let us explore how it is?"/>
+        </div>
+      </div>
     </div>
   )
 }
