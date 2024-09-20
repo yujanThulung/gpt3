7b48b0d907105d58687c088f906dc373c0256709
yujan Thulung <yujanrai07@gmail.com>
2024-09-20 14:49:00 +0545

    Project complete

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <>
+            <div className="gpt3__footer section__padding">
+                <div className="gpt3__footer-heading">
+                    <h1 className="gradient__text">
+                        Do you want to step in to the future before others.
+                    </h1>
+                </div>
+                <div className="gpt3__footer_btn">
+                    <p>Request Early Access</p>
+                </div>
 
-export default Footer
+                <div className="gpt3__footer-links">
+                    <div className="gpt3__footer-links_logo">
+                        <img src={gpt3Logo} alt="logo" />
+                        <p>Kathmandu Nepal</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Links</h4>
+                        <p>Overons</p>
+                        <p>Social Media</p>
+                        <p>Counter</p>
+                        <p>Contacats</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Company</h4>
+                        <p>Overons</p>
+                        <p>Social Media</p>
+                        <p>Counter</p>
+                        <p>Contacats</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Get in touch</h4>
+                        <p>Overons</p>
+                        <p>Social Media</p>
+                        <p>Counter</p>
+                        <p>Contacats</p>
+                    </div>
+                </div>
+                <div className="gpt3__footer-copyright">
+                <p>&copy; 2024 All right reserve to Yujan Thulung</p>
+            </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
